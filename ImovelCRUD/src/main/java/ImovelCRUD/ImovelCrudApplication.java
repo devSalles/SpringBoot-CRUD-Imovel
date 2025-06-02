3e768554a14db916c4ab1911266e586dea541853
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2025-06-02 12:39:31 -0300

    Primeiro Commit

---
+++ ImovelCRUD/src/main/java/ImovelCRUD/ImovelCrudApplication.java
@@ -0,0 +1,13 @@
+package ImovelCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImovelCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImovelCrudApplication.class, args);
+	}
+
+}
---
+++ ImovelCRUD/src/test/java/ImovelCRUD/ImovelCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ImovelCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImovelCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
