dcf1f5c00df60454b9e0d07336ed905b13e40be2
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2025-06-03 12:09:04 -0300

    Refatoramento da entidade e DTO

--- ImovelCRUD/src/main/java/ImovelCRUD/dto/ImovelDTO.java
+++ ImovelCRUD/src/main/java/ImovelCRUD/dto/ImovelDTO.java
@@ -1,4 +1,18 @@
 package ImovelCRUD.dto;
 
-public record ImovelDTO() {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ImovelDTO(
+        @NotBlank String logradouro,
+        @NotBlank String numero,
+        @NotBlank String bairro,
+        @NotBlank String cep,
+        @NotBlank String regiao,
+        @NotBlank String cidade,
+        @NotBlank String estado,
+        String complemento,
+        @NotBlank String tipo,
+        @NotNull Double valor,
+        @NotBlank String disponibilidade
+){}
--- ImovelCRUD/src/main/java/ImovelCRUD/model/ImovelModel.java
+++ ImovelCRUD/src/main/java/ImovelCRUD/model/ImovelModel.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Locale;
+
 @Entity
 @Table(name = "tb__imovel")
 @Getter
@@ -17,18 +19,36 @@ public class ImovelModel {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String endereco;
+    @Column(nullable = false,length = 130)
+    private String logradouro;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "num_imovel",length = 10)
+    private String numero;
+
+    @Column(nullable = false,length = 60)
+    private String bairro;
+
+    @Column(nullable = false, name = "cep_imovel",length = 10)
     private String cep;
 
-    @Column(nullable = false)
+    @Column(nullable = false,length = 55)
+    private String regiao;
+
+    @Column(nullable = false,length = 120)
+    private String cidade;
+
+    @Column(nullable = false,length = 35)
+    private String estado;
+
+    @Column(length = 60)
+    private String complemento;
+
+    @Column(nullable = false,name = "tipo_imovel",length = 55)
     private String tipo;
 
-    @Column(nullable = false)
+    @Column(nullable = false,precision = 13,scale = 4)
     private Double valor;
 
-    @Column(nullable = false)
+    @Column(nullable = false,length = 25)
     private String diponibilidade;
 }
